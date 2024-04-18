88a54d67bc9b176db5904019f0544e7841ca94af
Hiago Agostinho <97325933+hiago-agostinho@users.noreply.github.com>
2024-04-18 13:21:54 -0300

    Adicionando input de matricula

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  email: string = '';
 
+  constructor() { }
 }
