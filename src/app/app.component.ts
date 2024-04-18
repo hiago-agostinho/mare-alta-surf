064fb37c3df4c4472df4281ae6b8f854fdb7dcf0
Hiago Agostinho <97325933+hiago-agostinho@users.noreply.github.com>
2024-04-18 16:04:54 -0300

    [WIP]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { DescricaoComponent } from './descricao/descricao.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    RouterOutlet, HomeComponent
+    RouterOutlet, HomeComponent, DescricaoComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
